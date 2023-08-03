91997613b8db7649c7957d7413726f6effa932ef
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-08-03 15:08:48 -0600

    set up NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import CategoriesList from "./CategoriesList";
-
+import NavBar from "./NavBar";
 
 function App() {
   const [categories, setCategories] = useState ("")
