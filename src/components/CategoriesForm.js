667c6f65a0cd9155870d500e83fc4b3922bcee79
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-08-08 22:25:36 -0600

    setting up post

--- src/components/CategoriesForm.js
+++ src/components/CategoriesForm.js
@@ -1,8 +1,34 @@
 import { useEffect, useState } from "react";
 
 function CategoriesForm(){
-    const [name, setName] = useState("")
-    const [img, setImg] = useState("")
+    const [formData, setFormData] = useState({
+        name: "",
+        img: ""
+})
+
+
+    function handleChange(e){
+        setFormData({
+            ...formData,
+            [e.target.name] :e.target.value
+    })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        
+    //     useEffect(() => {
+    //         fetch ("http://localhost:9292/categories", {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json"
+    //             },
+    //             body: JSON.stringify()
+    //             })
+    //           .then(r => r.json())
+    //           .then(data => console.log(data))
+    //       }, [])
+    }
 
 
     return (
@@ -11,18 +37,20 @@ function CategoriesForm(){
             <h2>Add New Category</h2>
             <form>
                 <input 
-                    onChange={(e) => setName(e.target.value)}
-                    value={name} 
+                    onChange={handleChange}
+                    value={formData.name} 
                     type="text" 
                     name="name" 
                     placeholder="Category name" />
                 <input 
-                    onChange={(e) => setImg(e.target.value)}
-                    value={img} 
+                    onChange={handleChange}
+                    value={formData.img} 
                     type="text" 
                     name="image" 
                     placeholder="Image URL" />
-                <button type="submit">Add Category</button>
+                <button 
+                    type="submit">Add Category</button>
+                    {/* onSubmit={handleSubmit} */}
             </form>
         </div>
     )
