a1b4cd74f7a51e89f5f8036feef01ba3bb19627f
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-08-08 21:56:59 -0600

    set state for category form

--- src/components/CategoriesForm.js
+++ src/components/CategoriesForm.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
+
 function CategoriesForm(){
+    const [name, setName] = useState("")
+    const [img, setImg] = useState("")
+
+
     return (
 
         <div className="NewCategory" >
             <h2>Add New Category</h2>
             <form>
-                <input  type="text" name="name" placeholder="Category name" />
-                <input type="text" name="image" placeholder="Image URL" />
+                <input 
+                    onChange={(e) => setName(e.target.value)}
+                    value={name} 
+                    type="text" 
+                    name="name" 
+                    placeholder="Category name" />
+                <input 
+                    onChange={(e) => setImg(e.target.value)}
+                    value={img} 
+                    type="text" 
+                    name="image" 
+                    placeholder="Image URL" />
                 <button type="submit">Add Category</button>
             </form>
         </div>
